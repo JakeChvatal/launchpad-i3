ede2caef3db54a8ba91212b69d072bbb66dcd607
jakechvatal <jakechvatal@gmail.com>
2019-06-18 16:54:06 -0400

    i3 commands are run based on config file spec

--- src/launchpad-linux.py
+++ src/launchpad-linux.py
@@ -4,6 +4,9 @@ import sys
 import threading
 import json
 
+CONFIG_FILE = 'config.json'
+LETTER_BUTTONS = {8: 'a', 24: 'b', 40: 'c', 56: 'd', 72: 'e', 88: 'f', 104: 'g', 120: 'h'}
+
 try:
     import launchpad_py as launchpad
 except ImportError:
@@ -43,7 +46,7 @@ class Subscription(threading.Thread):
 
         # Thread initialization
         threading.Thread.__init__(self)
-        self.start()
+        self.listen()
 
     def run(self):
         """
@@ -92,16 +95,24 @@ class LaunchpadOs:
     def __init__(self):
         self.lp = launchpad.Launchpad()
         self.mode = 'a'
+        self.lp.Open()
+        try:
+            self.json = self.read_json(CONFIG_FILE)
+        except: # TODO: find spefici exceptions
+            sys.exit("The configuration file could not be found or was not in the format specified.")
 
         try:
             self.lp.LedAllOn(0)
         except AttributeError:
             sys.exit("The launchpad is not plugged in. Please plug in the device and try again.")
 
-    # displays leds based on workspace
+    def read_json(self, filepath):
+        with open(filepath, encoding='utf-8-sig') as json_file:
+            return json.loads(json_file.read())
+
+    # displays leds based on the focused workspace
     def workspace_control(self, event, data, subscription):
         letter_buttons = {8: 'a', 24: 'b', 40: 'c', 56: 'd', 72: 'e', 88: 'f', 104: 'g', 120: 'h'}
-
         self.lp.LedAllOn(0)
 
         for workspace in data:
@@ -110,43 +121,65 @@ class LaunchpadOs:
             else:
                 self.lp.LedCtrlXY(workspace['num'] - 1, 0, 0, 1)
 
-        for key, val in letter_buttons:
-            if val == self.mode:
-                self.lp.LedCtrlXY(key, 0, 2, 0)
-            else:
-                self.lp.LedCtrlXY(key, 0, 1, 0)
-
     # runs i3 controls based on the button pressed
     def i3_menu(self, event, data, subscription):
-        print(data)
-        letter_buttons = {8: 'a', 24: 'b', 40: 'c', 56: 'd', 72: 'e', 88: 'f', 104: 'g', 120: 'h'}
+        if data[1] is True:
+            # controls workspaces
+            if 200 <= data[0] <= 207:
+                i3.command('workspace {}'.format(data[0] - 199))
 
-        if data[1] is True and 200 <= data[0] <= 207:
-            i3.command('workspace {}'.format(data[0] - 199))
+            # menu for a to h buttons
+            elif LETTER_BUTTONS.get(data[0]) is not None:
+                self.mode = LETTER_BUTTONS.get(data[0])
+                self.refresh_letter_buttons()
+                self.refresh_grid()
 
-        # menu for a to h buttons
-        elif letter_buttons.get(data[0]) is not None:
-            self.mode = letter_buttons.get(data[0])
-            self.lp.LedCtrlString(letter_buttons.get(data[0]), 1, 1, 50)
-            self.lp.LedAllOn(0)
-
-        # menu for the normal buttons: behavior changes depending on menu and config file
-        elif data[1] == True and self.mode == 'a':
-            coord = [data[0] / 16 + 1, data[0] % 16]
-            print(coord)
-
-            if data[0] >= 64:
-                self.lp.LedCtrlRaw(data[0], 0, 2)
-                i3.command('exec termite')
+            # menu for the normal buttons: behavior changes depending on menu and config file
             else:
-                self.lp.LedCtrlRaw(data[0], 2, 0)
-                i3.command('kill')
+                coord = [int(data[0]/16), data[0] % 16]
+                i3.command(self.json[self.mode][coord[0]][coord[1]])
+
+    # sets the grid according to the current mode
+    def refresh_grid(self):
+        try:
+            grid = self.json[self.mode]
+        except KeyError:
+            print("This button does not have any configuration associated with it.")
+            self.reset_grid()
+            return
+
+        for i in range(0, 8):
+            for j in range(0, 8):
+                if grid[i][j] is not None and grid[i][j] != '':
+                    self.lp.LedCtrlXY(i, j + 1, 1, 1)
+                else:
+                    self.lp.LedCtrlXY(i, j + 1, 0, 0)
+
+    # resets the colors of the main grid
+    def reset_grid(self):
+        for i in range(0, 8):
+            for j in range(0, 8):
+                self.lp.LedCtrlXY(i, j + 1, 0, 0)
+
+    # refreshes the letter buttons to their proper values
+    def refresh_letter_buttons(self):
+        for key, val in LETTER_BUTTONS.items():
+            if val == self.mode:
+                self.lp.LedCtrlRaw(key, 2, 0)
+            else:
+                self.lp.LedCtrlRaw(key, 1, 1)
 
     # Starts the event listeners responsible for detecting button changes on the Launchpad.
     def main(self):
         if self.lp.Open():
-            self.workspace_control(False, i3.get_workspaces(), False)
 
+            # initial lighting
+            self.workspace_control(False, i3.get_workspaces(), False)
+            self.i3_menu(None, [0, 0], None)
+            self.refresh_letter_buttons()
+            self.refresh_grid()
+
+            # subscribe to events
             button_monitor = Subscription(self.i3_menu, self.lp)
             workspace_monitor = i3.Subscription(self.workspace_control, 'workspace')
 
